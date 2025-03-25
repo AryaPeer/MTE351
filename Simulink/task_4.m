e61001724d8c3fe90d10f0d6fc4e98f2f419abcc
Monk-Mental <arya.peer@gmail.com>
2025-03-24 20:06:25 -0400

    Reorganization + Updated Simulink

---
+++ Simulink/task_4.m
@@ -0,0 +1,27 @@
+clc;
+clear all;
+
+%% Define system parameters
+M = 35;            
+Mu = 66; 
+Mb = 1.5;
+Lg = 1; 
+Lb = 1;
+Ig_w = 0.02;       
+Ig_u = 20;         
+R = 0.127;         
+g = 9.81;            
+
+% Thrust force is fixed but need to be found
+Ft = 25;   % placeeholder value  
+beta = 0.18;
+
+%% Define Constants shown in the block diagram
+C1 = M + Ig_w/R^2 + Mu;
+C2 = Mu * Lg;
+C5 = Mu * Lg;
+C4 = Mu * Lg^2 + Ig_u;
+% C6 = -1 * Mu * g * Lg * (theta + beta);   
+
+
+
